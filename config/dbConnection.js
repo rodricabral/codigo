765af74b6c35b2645adc3e76185d497bee83e451
rodricabral <rc3320451@gmail.com>
2024-12-07 20:46:23 -0300

    rrrrrrr

--- config/dbConnection.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const dbConnection = new Sequelize('uade_test', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306,
-});
-
-module.exports = {dbConnection}
